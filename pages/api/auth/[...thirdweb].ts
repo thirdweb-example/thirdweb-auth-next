1eb5418cebc5db9f7b85877e776a40ae25b8e078
adam-maj <mr.adam.maj@gmail.com>
2022-08-31 14:00:58 -0700

    Add ADMIN_PRIVATE_KEY

--- pages/api/auth/[...thirdweb].ts
+++ pages/api/auth/[...thirdweb].ts
@@ -1,7 +1,7 @@
 import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
-  privateKey: process.env.THIRDWEB_AUTH_PRIVATE_KEY || "",
+  privateKey: process.env.ADMIN_PRIVATE_KEY || "",
   domain: "localhost:3000",
 });
 
