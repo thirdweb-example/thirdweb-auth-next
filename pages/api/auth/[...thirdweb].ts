8cac361e9cee951fde44ff860680a7b2b32971f3
adam-maj <mr.adam.maj@gmail.com>
2023-01-22 16:19:58 -0500

    Update private key wallet import

--- pages/api/auth/[...thirdweb].ts
+++ pages/api/auth/[...thirdweb].ts
@@ -1,5 +1,5 @@
 import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
-import { PrivateKeyWallet } from "@thirdweb-dev/wallets";
+import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
   wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
