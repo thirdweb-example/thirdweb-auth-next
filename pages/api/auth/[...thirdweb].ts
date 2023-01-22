26b0609e610d7ed314841fe6888ce8c571712e92
adam-maj <mr.adam.maj@gmail.com>
2023-01-22 16:45:27 -0500

    Match example

--- pages/api/auth/[...thirdweb].ts
+++ pages/api/auth/[...thirdweb].ts
@@ -2,8 +2,8 @@ import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
 import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
-  wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
   domain: process.env.NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN || "",
+  wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
 });
 
 export default ThirdwebAuthHandler();
