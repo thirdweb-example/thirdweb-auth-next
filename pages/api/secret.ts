53735fef3b80a09bf8cfcbc76198bcb94ff91ae0
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-01-27 13:24:07 +0530

    feat: update example (#2)

--- pages/api/secret.ts
+++ pages/api/secret.ts
@@ -5,7 +5,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "GET") {
     return res.status(400).json({
       message: "Invalid method.",
-    })
+    });
   }
 
   const user = await getUser(req);
@@ -13,12 +13,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!user) {
     return res.status(401).json({
       message: "Not authorized.",
-    })
+    });
   }
 
   return res.status(200).json({
-    message: "This is a secret... don't tell anyone."
+    message: "This is a secret... don't tell anyone.",
   });
-}
+};
 
 export default handler;
