f1063a66868f360774391f5d24df2e3a4e43c97a
Jonas Daniels <jonas.daniels@outlook.com>
2024-03-20 16:30:58 -0700

    feat: Update ThirdwebProvider props in page components

--- src/app/jwt-cookie/page.tsx
+++ src/app/jwt-cookie/page.tsx
@@ -2,12 +2,16 @@
 import type { NextPage } from "next";
 import { ConnectButton } from "thirdweb/react";
 import { LoginButton } from "./components/login-button";
+import { client } from "../../lib/client";
 
 const JWTCookiePage: NextPage = () => {
   return (
     <div>
       <h2>Step 1: Connect Wallet</h2>
-      <ConnectButton />
+      <ConnectButton
+        client={client}
+        appMetadata={{ name: "auth test app", url: "https://thirdweb.com" }}
+      />
       <h2>Step 2: Sign the login payload</h2>
       <LoginButton />
     </div>
